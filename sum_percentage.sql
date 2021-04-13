ccfac81b5841a908c7094e14be62defc1a347448
tony-tomy <61079333+tony-tomy@users.noreply.github.com>
2021-04-13 12:47:54 +0800

    Create sum_percentage.sql

---
+++ sum_percentage.sql
@@ -0,0 +1,7 @@
+'''
+SQL Code to find the percentage of amount spent on each order
+'''
+SELECT product_id, price * quantity as total,
+CONCAT(ROUND(100 *(price * quantity)/SUM(price * quantity) OVER( PARTITION BY product_id)::FLOAT), '%') as percentage 
+FROM public.order
+ORDER BY product_id;
